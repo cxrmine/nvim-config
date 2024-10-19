71b5c5e18ccb9bbd40fe3a88aeba08a3e55b11fa
amaryllis <amaryllismain09@gmail.com>
2024-10-19 21:56:11 +0200

    Feat(plug): Added Themery plugin

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 require("config.lazy")
 require("options.keymaps")
 require("options.ui")
-
-vim.cmd("colorscheme kanagawa")
--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -4,3 +4,6 @@ local set = vim.keymap.set
 -- opens the vim NETRW file explorer
 set("n", "<leader>pv", "<CMD>Ex<CR>", opts)
 
+-- opens the colorscheme switcher window
+set("n", "<leader>cs", "<CMD>Themery<CR>", opts)
+
