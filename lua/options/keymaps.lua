5982026f77de9e92842d92479187fa4819cc6d7f
carmine-git <amaryllismain09@gmail.com>
2024-10-23 13:08:59 +0200

    Chore(keymaps): Added keymaps for telescope

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -7,3 +7,10 @@ set("n", "<leader>pv", "<CMD>Ex<CR>", opts)
 -- opens the colorscheme switcher window
 set("n", "<leader>cs", "<CMD>Themery<CR>", opts)
 
+-- source: https://github.com/nvim-telescope/telescope.nvim
+local builtin = require("telescope.builtin")
+set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+-- source: https://github.com/nvim-telescope/telescope.nvim
