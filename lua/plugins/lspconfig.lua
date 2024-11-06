b5761dd841e04efa6292756d25f447f85f2268ad
carmine-git <amaryllismain09@gmail.com>
2024-11-06 20:10:44 +0100

    cleaning

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,37 +1,43 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufNewFile", "BufReadPre", "BufReadPost" },
-	config = function()
-		local cfg = require("lspconfig")
-		cfg.lua_ls.setup({
-			on_init = function(client)
-				if client.workspace_folders then
-					local path = client.workspace_folders[1].name
-					if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
-						return
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufNewFile", "BufReadPre", "BufReadPost" },
+		config = function()
+			local cfg = require("lspconfig")
+			cfg.lua_ls.setup({
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+							return
+						end
 					end
-				end
-				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						version = "LuaJIT",
-					},
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME,
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+						},
+					})
+				end,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
 						},
 					},
-				})
-			end,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
 				},
-			},
-		})
-		cfg.clangd.setup({})
-		cfg.pyright.setup({})
-	end,
+			})
+			cfg.clangd.setup({})
+			cfg.pyright.setup({})
+		end,
+	},
 }
