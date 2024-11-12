1d817cf4d6ed3e20493b63bc0bdfaf6511180530
talal <malkawitalal@gmail.com>
2024-11-12 08:34:30 +0100

    changed ft header conf

--- lua/plugins/ft_header.lua
+++ lua/plugins/ft_header.lua
@@ -1,11 +1,6 @@
 return {
 	"Diogo-ss/42-header.nvim",
 	cmd = "Stdheader",
-	keys = {
-		"<leader>ft",
-		"<cmd>Stdheader<CR>",
-		desc = "[f]orty-[t]wo | adds the 42 header in C file"
-	},
 	opts = {
 		default_map = false,
     	auto_update = true,
