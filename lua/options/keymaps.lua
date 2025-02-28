5d8028e866836dee72717ec38a80078388767122
tmalkawi <tmalkawi@student.42.fr>
2025-02-28 14:43:51 +0100

    refactor: keybinds

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -1,18 +1,3 @@
-local opts = { silent = true, noremap = true }
 local set = vim.keymap.set
-local telescope = require("telescope.builtin")
 
-set("n", "<leader>ft", function()
-	vim.cmd("Stdheader")
-end, { desc = "[f]orty-[t]wo | adds 42 header on C files" })
-set("n", "<leader>to", function()
-	vim.cmd("NvimTreeOpen")
-end, { desc = "[t]ree-[o]pen | open the file tree view" })
-set("n", "<leader>tc", function()
-	vim.cmd("NvimTreeClose")
-end, { desc = "[t]ree-[c]lose | close the file tree view" })
 set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,34 +2,14 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"<leader>ff",
-			function()
-				require("telescope.builtin").find_files()
-			end,
-			desc = "Telescope find files",
-		},
-		{
-			"<leader>fg",
-			function()
-				require("telescope.builtin").live_grep()
-			end,
-			desc = "Telescope live grep",
-		},
-		{
-			"<leader>fb",
-			function()
-				require("telescope.builtin").buffers()
-			end,
-			desc = "Telescope buffers",
-		},
-		{
-			"<leader>fh",
-			function()
-				require("telescope.builtin").help_tags()
-			end,
-			desc = "Telescope help tags",
-		},
-	},
+	keys = function(_, keys)
+		keys = keys or {}
+		local bltin = require("telescope.builtin")
+		return vim.tbl_deep_extend("force", keys, {
+			{ "<leader>ff", bltin.find_files, desc = "Telescope find files" },
+			{ "<leader>fg", bltin.live_grep, desc = "Telescope live grep", },
+			{ "<leader>fb", bltin.buffers, desc = "Telescope buffers", },
+			{ "<leader>fh", bltin.help_tags, desc = "Telescope help tags" },
+		})
+	end,
 }
