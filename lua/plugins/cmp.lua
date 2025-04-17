d7c23fe75dab6430eacdd59cf3f50ab26d0619f9
Talal MALKAWI <malkawitalal@gmail.com>
2025-04-17 20:16:13 +0200

    feat: add more configs for blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,17 +1,23 @@
 return {
 	{
-		'saghen/blink.cmp',
+		"saghen/blink.cmp",
 		dependencies = {
-			"rafamadriz/friendly-snippets"
+			"rafamadriz/friendly-snippets",
 		},
 		version = "1.*",
 
 		---@module 'blink-cmp'
 		---@type blink.cmp.Config
 		opts = {
-			keymap = {
-				preset = 'enter',
-			}
-		}
-	}
+			keymap = { preset = "enter" },
+			fuzzy = { implementation = "lua" },
+			completion = {
+				trigger = { show_on_keyword = true },
+			},
+			menu = { draw = { columns = {
+				{ "kind_icon", "label", gap = 4, "kind"},
+			} } },
+		},
+	},
 }
+
