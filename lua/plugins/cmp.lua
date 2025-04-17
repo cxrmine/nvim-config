1ce7cce152ec03d79d2656c7a7638979f741eb02
Talal MALKAWI <malkawitalal@gmail.com>
2025-04-17 20:39:48 +0200

    feat: added treesitter lsp for list cmp menu

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,11 +13,25 @@ return {
 			fuzzy = { implementation = "lua" },
 			completion = {
 				trigger = { show_on_keyword = true },
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+						columns = {
+							{ "kind_icon", "label", gap = 4 },
+							{ "kind" },
+						},
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 400,
+				},
+				ghost_text = { enabled = true },
 			},
-			menu = { draw = { columns = {
-				{ "kind_icon", "label", gap = 4, "kind"},
-			} } },
+			signature = { enabled = true },
+			sources = {
+				default = { "lsp", "buffer", "snippets", "path" },
+			},
 		},
 	},
 }
-
