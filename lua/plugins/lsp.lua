a8a7897fc9ef9427b4b4b434e12ee166b6ae441e
Talal MALKAWI <malkawitalal@gmail.com>
2025-04-17 17:54:05 +0200

    chore: remove nvim-cmp plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,16 +7,8 @@ return {
 		"neovim/nvim-lspconfig",
 		event = { "BufNewFile", "BufReadPre", "BufReadPost" },
 		config = function()
-			local status, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-			if not status then
-				return
-			end
-			local capabilities = cmp_lsp.default_capabilities()
-
-			-- source: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
 			local cfg = require("lspconfig")
 			cfg.lua_ls.setup({
-				capabilities = capabilities,
 				on_init = function(client)
 					local path = client.workspace_folders[1].name
 					if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
@@ -38,82 +30,6 @@ return {
 				settings = {
 					Lua = {},
 				},
-			})
-			-- source: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
-
-			cfg.clangd.setup({ capabilities = capabilities })
-			cfg.pyright.setup({ capabilities = capabilities })
-			cfg.ast_grep.setup({ capabilities = capabilities })
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-vsnip",
-			"hrsh7th/vim-vsnip",
-			"onsails/lspkind.nvim",
-		},
-		event = { "BufNewFile", "BufReadPre", "BufReadPost" },
-		config = function()
-			local cmp = require("cmp")
-			local lspkind = require('lspkind')
-			cmp.setup({
-				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol",
-						maxwidth = {
-							menu = 50,
-							abbr = 50,
-						},
-						ellipsis_char = "...",
-						show_labelDetails = true,
-						before = function(entry, vim_item)
-							return vim_item
-						end,
-					}),
-				},
-				snippet = {
-					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "vsnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
 			})
 		end,
 	},
