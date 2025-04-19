d6b4c9e55f1534c9f9d1532641044bb5de594365
tmalkawi <tmalkawi@student.42.fr>
2025-04-19 09:04:24 +0200

    chore: remoove dup oil plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -105,17 +105,4 @@ return {
 			},
 		},
 	},
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		keys = {
-			{
-				"-",
-				"<CMD>Oil<CR>",
-				desc = "Open parent directory",
-			},
-		},
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		lazy = false,
-	},
 }
