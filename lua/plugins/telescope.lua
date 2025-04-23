03ebcf2740e812575e3706824a6f9957be995c60
tmalkawi <tmalkawi@student.42.fr>
2025-04-23 09:22:03 +0200

    feat: add deps for telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,18 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		{ "BurntSushi/ripgrep" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "sharkdp/fd" },
+	},
 	keys = function(_, keys)
 		keys = keys or {}
 		local bltin = require("telescope.builtin")
 		return vim.tbl_deep_extend("force", keys, {
 			{ "<leader>ff", bltin.find_files, desc = "Telescope find files" },
-			{ "<leader>fg", bltin.live_grep, desc = "Telescope live grep", },
-			{ "<leader>fb", bltin.buffers, desc = "Telescope buffers", },
+			{ "<leader>fg", bltin.live_grep, desc = "Telescope live grep" },
+			{ "<leader>fb", bltin.buffers, desc = "Telescope buffers" },
 			{ "<leader>fh", bltin.help_tags, desc = "Telescope help tags" },
 		})
 	end,
