e156374f28a6cac9b0cc308b5d8de6c29404a3c5
cxrmine <amaryllismain09@gmail.com>
2025-05-09 18:07:59 +0200

    style: stylua keymaps.lua

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -2,17 +2,30 @@ local keymap = vim.keymap
 local opts = { silent = true, noremap = true }
 
 -- split the window
-keymap.set("n", "ss", function() return vim.cmd("split") end, opts)
-keymap.set("n", "sv", function() return vim.cmd("vsplit") end, opts)
+keymap.set("n", "ss", function()
+	return vim.cmd("split")
+end, opts)
+keymap.set("n", "sv", function()
+	return vim.cmd("vsplit")
+end, opts)
 
 -- source current lua file
-keymap.set("n", "<leader>sc", function() return vim.cmd("source %") end, opts)
+keymap.set("n", "<leader>sc", function()
+	return vim.cmd("source %")
+end, opts)
 
 -- save and exit all
-keymap.set("n", "<leader>Q", function() return vim.cmd("wqa") end, opts)
+keymap.set("n", "<leader>Q", function()
+	return vim.cmd("wqa")
+end, opts)
 
 -- window navigation
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sl", "<C-w>l")
+
+-- open lazy plugin manager
+keymap.set("n", "L", function()
+	return vim.cmd("Lazy")
+end, opts)
