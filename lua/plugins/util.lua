2a5d7143f33201217415b20b1345b829c5a4edb8
cxrmine <amaryllismain09@gmail.com>
2025-06-16 12:55:08 +0200

    feat: group utils together

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,5 +1,29 @@
 return {
 	{
+		"folke/todo-comments.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "echasnovski/mini.icons" },
+		},
+		opts = {},
+	},
+	{
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			opts = {},
+			keys = {
+				{
+					"<leader>?",
+					function()
+						require("which-key").show({ global = false })
+					end,
+					desc = "Buffer Local Keymaps (which-key)",
+				},
+			},
+		},
+	},
+	{
 		"mistricky/codesnap.nvim",
 		build = "make",
 		keys = {
