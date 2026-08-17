b703c945b630d2d9a200a3bf5220a22e7922566b
tmalkawi02 <malkawitalal@gmail.com>
2026-08-17 12:21:22 +0200

    fix: correct version of treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main";
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
 	config = function()
 		local ts = require("nvim-treesitter")
 		ts.setup({
-			modules = {},
-			auto_install = true,
-			ensure_installed = {
-				"c",
-				"lua",
-				"cpp",
-				"make",
-				"comment",
-				"typescript",
-				"bash",
-				"vim",
-				"vimdoc",
-				"regex",
-				"markdown",
-				"markdown_inline",
-			},
-			sync_install = true,
-			ignore_install = { "javascript" },
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024
-					local s, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if s and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
 			install_dir = vim.fn.stdpath("data") .. "/site",
-			additional_vim_regex_highlighting = true,
 		})
+		ts.install({ "lua", "vim", "vimdoc", "cpp", "c", "markdown", "make" })
 	end,
 }
